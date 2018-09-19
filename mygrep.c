0f51b80f8679ebebb04f879476d53dbdbced1fb2
claire-chemutai <clairechemutai12@gmail.com>
2018-09-19 19:03:42 +0000

    Updated mygrep file

--- mygrep.c
+++ mygrep.c
@@ -1,22 +1,31 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-
-//int main(int argc, char * argv[]){
-int main(){
-	int i;
-	FILE * fpointer;
-	char *ret;
-	//for(i=1; i<argc+1; i++){
-		fpointer=fopen("file.txt","r");
-		char line[150];
-
-		while (!feof(fpointer)){
-			fgets(line,150,fpointer);
-			if (strstr(line, "foo")){
-                printf("%s\n", line);
-			}
-		}
-		fclose(fpointer);
-	//}
-	return 0;
+#include  <stdlib.h>
+#include <string.h>
+int main(int argc, char * argv[]){
+FILE * fpointer;	
+int i;
+if(argc<2){
+	printf("mygrep: searchterm [file ....]\n");
+	exit(1);
 }
+for(i=2;i<argc; i++){
+	fpointer=fopen(argv[i],"r");
+ 	if (fpointer){
+	char line[150];
+
+	while (!feof(fpointer)){
+		fgets(line,150,fpointer);
+		if (strstr(line, argv[1])){
+	                printf("%s\n", line);
+		}
+	}
+	fclose(fpointer);
+	}else {
+    		printf("mygrep: cannot open file\n");
+    		exit(1);
+	}
+}
+return 0;
+}
+
