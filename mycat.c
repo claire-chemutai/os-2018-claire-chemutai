979d9a8b04bf8c55d5b84569f015e338d8851047
claire-chemutai <claire-chemutai@users.noreply.github.com>
2018-09-20 12:14:50 +0000

    Comments made on the file

--- mycat.c
+++ mycat.c
@@ -2,17 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+*A program to read and print out the contents of a file
+*/
 int main(int argc, char * argv[]){
 int i;
 FILE * fpointer;
 if(argc<2){
 	exit(1);
 }
+	//loop around all the files
 	for(i=1; i<argc; i++){
+		//open a file
 		fpointer=fopen(argv[i],"r");
 		if(fpointer){
 		char line[500];
-
+		
+		//get all the lines in the file
 		while (fgets(line,500,fpointer)){
 			puts(line);
 		}
