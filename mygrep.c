c7f5e081880cb2d87c5d47e91d4293f3fd08b4d7
claire-chemutai <claire-chemutai@users.noreply.github.com>
2018-09-20 12:16:44 +0000

    Comments added on mygrep file

--- mygrep.c
+++ mygrep.c
@@ -2,14 +2,23 @@
 #include <stdio.h>
 #include  <stdlib.h>
 #include <string.h>
+
+/*
+*A program to search for terms in a file
+*/
 int main(int argc, char * argv[]){
 FILE * fpointer;	
 int i;
+	
+//if a filename and search term isnt stated
 if(argc<2){
 	printf("mygrep: searchterm [file ....]\n");
 	exit(1);
 }
+	
+//loop through all the file
 for(i=2;i<argc; i++){
+	//open each file
 	fpointer=fopen(argv[i],"r");
  	if (fpointer){
 	char line[150];
